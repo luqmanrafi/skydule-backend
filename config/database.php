31c1ed66999d1adc9495dd264718ba34e4c75ca3
ariel <ariel.rakadewa@gdn-commerce.com>
2025-05-02 20:18:52 +0700

    konfigurasi database baru

--- config/database.php
+++ config/database.php
@@ -112,9 +112,16 @@ return [
             // 'trust_server_certificate' => env('DB_TRUST_SERVER_CERTIFICATE', 'false'),
         ],
         'mongodb' => [
-            'driver' => 'mongodb',
-            'dsn' => env('DB_URI'),
-            'database' => 'skydule',
+            'driver'   => 'mongodb',
+            'dsn'      => env('DB_DSN', ''),
+            'host'     => env('DB_HOST', '127.0.0.1'),
+            'port'     => env('DB_PORT', 27017),
+            'database' => env('DB_DATABASE', 'forge'),
+            'username' => env('DB_USERNAME', 'forge'),
+            'password' => env('DB_PASSWORD', ''),
+            'options'  => [
+                'database' => env('DB_AUTHENTICATION_DATABASE', 'admin'), // optional
+            ],
         ],
 
     ],
@@ -152,7 +159,7 @@ return [
 
         'options' => [
             'cluster' => env('REDIS_CLUSTER', 'redis'),
-            'prefix' => env('REDIS_PREFIX', Str::slug(env('APP_NAME', 'laravel'), '_').'_database_'),
+            'prefix' => env('REDIS_PREFIX', Str::slug(env('APP_NAME', 'laravel'), '_') . '_database_'),
             'persistent' => env('REDIS_PERSISTENT', false),
         ],
 
