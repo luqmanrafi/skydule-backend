f0e5984c04ced228f6ac367603de7b43fdf496dc
ariel <ariel.rakadewa@gdn-commerce.com>
2025-05-26 03:25:10 +0700

    fix: id auto generate & prevent race condition

--- routes/web.php
+++ routes/web.php
@@ -11,14 +11,16 @@ use App\Models\Matakuliah;
 Route::get('/home', function () {   
     return view('home');
 });
-Route::get('/matakuliah', [MatakuliahController::class, 'index'])->name('matakuliah.index');
-Route::get('/matakuliah/create', function () {
-    return view('matakuliah.create');
-})->name('matakuliah.create');
-Route::post('/matakuliah', [MatakuliahController::class, 'store'])->name('matakuliah.store');
-Route::get('/matakuliah/{id}/edit', [MatakuliahController::class, 'edit'])->name('matakuliah.edit');
-Route::put('/matakuliah/{id}', [MatakuliahController::class, 'update'])->name('matakuliah.update');
-Route::delete('/matakuliah/{id}', [MatakuliahController::class, 'destroy'])->name('matakuliah.destroy');
+Route::prefix('/admin')->group(function () {
+    Route::get('/matakuliah', [MatakuliahController::class, 'index'])->name('matakuliah.index');
+    Route::get('/matakuliah/create', function () {
+        return view('matakuliah.create');
+    })->name('matakuliah.create');
+    Route::post('/matakuliah', [MatakuliahController::class, 'store'])->name('matakuliah.store');
+    Route::get('/matakuliah/{id}/edit', [MatakuliahController::class, 'edit'])->name('matakuliah.edit');
+    Route::put('/matakuliah/{id}', [MatakuliahController::class, 'update'])->name('matakuliah.update');
+    Route::delete('/matakuliah/{id}', [MatakuliahController::class, 'destroy'])->name('matakuliah.destroy');
+});
 
 Route::get('/auth/google/redirect', function () {
     return Socialite::driver('google')->redirect();
